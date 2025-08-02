8da354b65cbb2b1ca975699b60dad01d3dc4d65e
Naiara Martineli <naahmartineli@gmail.com>
2025-08-01 22:53:20 -0300

    Atualizações visuais para responsividade com media query atualizada

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import AppRoutes from './routes/privateRoute'; // Use o componente que já criou
-import './css/global.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AppRoutes from "./routes/privateRoute"; // Use o componente que já criou
+import "./css/global.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <AppRoutes />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<AppRoutes />
+	</React.StrictMode>
 );
